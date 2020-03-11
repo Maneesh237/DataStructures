d95e78ecd7c9778b7591a74dd06fd9adc8c8ecd3
Maneesh237 <54877415+Maneesh237@users.noreply.github.com>
2020-03-11 16:57:27 +0530

    Add files via upload

---
+++ bitmanipulation/SecondProgram.java
@@ -0,0 +1,52 @@
+import java.util.*;
+class Main
+{
+    static Scanner s = new Scanner(System.in);
+	int count=0;
+	void minimumFlips(String a,String b,String c)
+	{
+		for(int i=0;i<c.length();i++)
+		{
+			if(c.charAt(i)=='0')
+			{
+				if(a.charAt(i)=='1' && b.charAt(i)=='1')
+					{
+						count=count+2;
+					}
+				else if(a.charAt(i)=='1' && b.charAt(i)=='0')
+					count++;
+				else if(a.charAt(i)=='0' && b.charAt(i)=='1')
+					count++;
+			}
+			else if(c.charAt(i)=='1')
+			{
+				if(a.charAt(i)=='0' && b.charAt(i)=='0')
+					{
+						count++;
+					}
+			}
+		}
+		System.out.println(count);
+	}
+	String addingLeadingZeroes(int m)
+	{
+		String s1="";
+		for(int i=0;i<m;i++)
+			s1=s1+0;
+		return s1;
+	}
+	public static void main(String[] args) {
+		Main m = new Main();
+	    int a =s.nextInt();
+	    int b= s.nextInt();
+	    int c= s.nextInt();
+	    String ainbinary = Integer.toBinaryString(a);
+	    String binbinary = Integer.toBinaryString(b);
+		String cinbinary = Integer.toBinaryString(c);
+		int max=(ainbinary.length()>=binbinary.length())?(ainbinary.length()>=cinbinary.length()?ainbinary.length():binbinary.length()):(binbinary.length()>=cinbinary.length()?binbinary.length():cinbinary.length());
+		ainbinary=m.addingLeadingZeroes(max-ainbinary.length())+ainbinary;
+		binbinary=m.addingLeadingZeroes(max-binbinary.length())+binbinary;
+		cinbinary=m.addingLeadingZeroes(max-cinbinary.length())+cinbinary;
+		m.minimumFlips(ainbinary,binbinary,cinbinary);
+	}
+}
